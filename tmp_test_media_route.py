926dddd835279c4c5aef11b17f17f631cb43dc53
Salioubangs <helali22472@gmail.com>
2026-05-22 12:38:27 +0000

    Unify business file access through Django file views

--- tmp_test_media_route.py
+++
@@ -1,5 +0,0 @@
-from django.test import Client
-
-response = Client().get("/media/depenses/justificatifs/hand-drawn-fake-autograph-sample-editable-stroke-signature-vector.jpg")
-print(response.status_code)
-print(response.get("Content-Type"))
